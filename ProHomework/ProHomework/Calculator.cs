008620f9b2d150714ed04d30eaf2441a1e306576
Karyna <138473407+CapeCoral@users.noreply.github.com>
2023-07-25 21:26:18 +0300

    homeworks

---
+++ ProHomework/ProHomework/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace ProHomework
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Деление на ноль не допустимо");
+            }
+
+            return a / b;
+        }
+
+        public int Power(int a, int exponent)
+        {
+            if (exponent < 0)
+            {
+                throw new ArgumentException("Отрицательные показания степени не поддерживаются");
+            }
+
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= a;
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ ProHomework/ProHomework/Program.cs
@@ -0,0 +1,39 @@
+﻿using ProHomework;
+using System;
+using System.Text;
+
+namespace Lesson
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator calculator = new Calculator();
+
+            int resultAdd = calculator.Add(5, 3); // Результат: 8
+            int resultSubtract = calculator.Subtract(10, 4); // Результат: 6
+            int resultMultiply = calculator.Multiply(2, 6); // Результат: 12
+
+            try
+            {
+                int resultDivide = calculator.Divide(10, 5); // Результат: 2
+                Console.WriteLine("Результат ділення: " + resultDivide);
+            }
+            catch (ArgumentException)
+            {
+                throw new Exception();
+            }
+            
+            int resultPower = calculator.Power(2, 3); // Результат: 8, тобто 2^3
+
+            Console.WriteLine("Результат додавання: " + resultAdd);
+            Console.WriteLine("Результат віднімання: " + resultSubtract);
+            Console.WriteLine("Результат множення: " + resultMultiply);
+            
+            Console.WriteLine("Результат піднесення до степеня: " + resultPower);
+
+            Console.ReadKey();
+
+        }
+    }
+}
