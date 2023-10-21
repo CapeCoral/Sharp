6af44597fcfee75d5ff9239ce51774c8652f0451
Karyna <138473407+CapeCoral@users.noreply.github.com>
2023-10-21 13:00:33 +0300

    homework 11

---
+++ ProHomework/TheNote/TheNote/Controllers/NoteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TheNote.Models;
+
+namespace TheNote.Controllers;
+
+public class NoteController : Controller
+{
+    private List<Note> notes = new List<Note>();
+
+    public ActionResult Index()
+    {
+        // Отримуємо список усіх заміток та відображаємо їх на сторінці
+        return View(notes);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Note note)
+    {
+        // Додаємо нову замітку до списку
+        note.CreatedDate = DateTime.Now;
+        notes.Add(note);
+        return RedirectToAction("Index");
+    }
+}
+
---
+++ ProHomework/TheNote/TheNote/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace TheNote.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public List<string> Tags { get; set; }
+}
